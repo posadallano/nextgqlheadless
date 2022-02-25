1cf4b9fdbb4a9de3318303593a9242b91bb7daf3
Andres Posada <posadallano2@gmail.com>
2022-02-24 19:10:06 -0500

    Get post slug and build query to fetch post data

---
+++ pages/posts/[slug].js
@@ -0,0 +1,38 @@
+export default function Post(data) {
+    
+}
+
+export async function getStaticProps(context) {
+    const res = await fetch('http://localhost/wpnextgql/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            query: `
+                query SinglePost($id: ID!, $idType: PostIdType!) {
+                    post(id: $id, idType: $idType) {
+                        title
+                        slug
+                        content
+                        featuredImage {
+                            node {
+                                sourceUrl
+                            }
+                        }
+                    }
+                }
+            `,
+            variables: {
+                id: context.params.slug,
+                idType: 'SLUG'
+            }
+        })
+    })
+
+    const json = await res.json()
+
+    return {
+        props: {
+            post: json.data.post,
+        },
+    }       
+}
